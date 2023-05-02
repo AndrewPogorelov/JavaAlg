b4a47bcd44c9a09b50e9e0367d6c7ee0b5150a7c
AndreyPogorelov <pogorelov1441@gmail.com>
2023-05-02 12:17:30 +0300

    added 19 task

--- java_pogorelov_19/src/Function.java
+++ java_pogorelov_19/src/Function.java
@@ -6,7 +6,7 @@
 //        Расчитать систему уравнений. Пр. func(10) -> func(10) = func(5) [правило 3] ->
 //        func(5) = func(2) + func(3) [правило 4] -> func(2) = func(1) [правило 3] ->
 //        func(1) = 1 [правило 2] -> funtc(3) = func(1) + func(2) [правило 4] ->
-//        func(1) и func (2) посчитаны и равны 1. Результат func(10) = func(5) = func(2) + func(3) = 1+2 = 3.
+//        func(1) и func (2) посчитаны и равны 1. Результат func(10) = func(5) = func(2) + func(3) = 1+2 = 3
 public class Function {
 
     static int calculateFunction(int n) {
